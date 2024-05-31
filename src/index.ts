154100c855d84b1d2f86eccde81395432fb0510b
sing3demons <sing3demons@live.com>
2024-05-31 15:48:17 +0700

    chore: Import Logger module and export additional classes and functions
    This commit adds the Logger module to the index.js file and exports additional classes and functions from the logger.js file. This allows for easier access to the Logger module and other commonly used modules.

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,8 @@ import { default as express, Application, Request, Response, NextFunction } from
 import { z } from 'zod'
 import { default as Context } from './context.js'
 import { default as Server } from './server.js'
+import { default as Logger, ContextType, ILogger, LoggerType, ignoreCase } from './logger.js'
 export { IContext } from './context.js'
-export { Context, Server }
+export { Context, Server, Logger, ContextType, ILogger, LoggerType, ignoreCase }
 export { IRoute, MyRouter, NotFoundError, TypeRoute, UnauthorizedError, globalErrorHandler } from './route.js'
 export { z, express, Application, Request, Response, NextFunction }
